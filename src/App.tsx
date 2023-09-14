dd94f96d2f78d4fa3f326ceabde95a4a93a0d66f
Luan Bruno <luanbrunomelo03@gmail.com>
2023-09-14 00:58:08 -0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Button } from "./components/ui/button";
 import { Github } from "lucide-react";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return (
-    <div className="p">
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">upload.ai</h1>
 
@@ -15,7 +16,41 @@ export function App() {
             Github
           </Button>
         </div>
+
+
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA"
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Rultado derado pela IA..."
+              readOnly
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do video selecionado.
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border w-full"
+            >
+              Carregar Vídeo
+            </label>
+            <input type="file" id="video" accept="video/mp4" className="sr-only" />
+          </form>
+        </aside>
+      </main>
     </div>
   )
 }
